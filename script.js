b7be3c3b87c5c105e6cf55e6f7cf6676e6a7f647
“Alejo <neopilatos@gmail.com>
2024-07-31 11:11:32 -0500

    add values

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 // Obtener elementos del DOM
-const slider = document.getElementById('myRange');
+//sliders
+const sliderValues = document.getElementById('myRange');
 const sliderMonths = document.getElementById('rangeMonths');
-const output = document.getElementById('demo');
+
+const amount = document.getElementById('amount');
+const amountReport = document.getElementById('amountReport');
 const total = document.getElementById('total');
 const monthsLabel = document.getElementById('monthsLabel');
+const monthReport = document.getElementById('monthReport');
 
-// Establecer el valor inicial del slider
-const initialValue = 40000;
-slider.value = initialValue;
+// inicializar valores
+const initialValue = 40000; // initial value on amount slider
+const InteresEA = 26.8242; // Interes Efectivo Anual
+const InteresMensual = convertEAtoMV(InteresEA); // Interes MV
+
+
+
+sliderValues.value = initialValue;
 
 // Función para actualizar los valores en el DOM
 function updateValues(value, isMonths = false) {
-  console.log(value);
-  output.innerHTML = value;
+  amount.innerHTML = value;
+  amountReport.innerHTML = Number(value).toLocaleString();
   total.innerHTML = value * 10;
   if (isMonths) {
     monthsLabel.innerHTML = value;
   }
 }
 
+function convertEAtoMV(InteresEA){
+    let ea = InteresEA/100;
+    let em = (Math.pow((1 + ea), (1/12))-1)*100;
+    return em.toFixed(2);
+}
+
 // Event listener para el slider principal
-slider.addEventListener('input', function() {
+sliderValues.addEventListener('input', function() {
   updateValues(this.value);
 });
 
 // Event listener para el slider de meses
 sliderMonths.addEventListener('input', function() {
-  updateValues(this.value, true);
+    monthReport.innerHTML = this.value;
 });
