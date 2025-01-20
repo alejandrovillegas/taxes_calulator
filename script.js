3a0f9c100d936487d486b680fb991e9acccf1f48
Alejandro Villegas <neopilatos@gmail.com>
2025-01-20 13:14:36 -0500

    Agregar formato de moneda al slider y mejorar la actualización de valores en el reporte de crédito

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const btn = document.getElementById("openModalBtn");
 // Obtener el elemento <span> que cierra el modal
 const span = document.getElementsByClassName("close")[0];
 
-
 //docs: https://docs.google.com/spreadsheets/d/17cvJi5X5llrzgnoR5NtKKWacb7Tj5DbQ/edit?gid=1531492329#gid=1531492329
 // https://www.keypago.com/solicitar-cupo-simulador/
 // https://www.rapicredit.com/credito-para-ropa/
@@ -30,6 +29,7 @@ const span = document.getElementsByClassName("close")[0];
 // Inicializar valores
 let initialValue = 40000; // Valor inicial del slider de cantidad
 let initialMonths = 1; // Valor inicial del slider de meses
+let rawValue = 40000; // Valor sin formato
 const InteresEA = 26.8242; // Interés Efectivo Anual
 const interestMV = "2.0"; // Interés Mensual Vencido
 const InteresMensual = convertEAtoMV(InteresEA); // Interés Mensual Vencido
@@ -59,24 +59,26 @@ function calcularPMT(tasaInteresAnual, numPagos, montoPrestamo) {
 
 // Función para actualizar los valores en el DOM
 function updateValues() {
-  const service = initialValue * 0.1;
+  const service = rawValue * 0.1;
   const totalAmount = Math.round(
-    calcularPMT(InteresMensual, initialMonths, initialValue) +
+    calcularPMT(InteresMensual, initialMonths, rawValue) +
       service / initialMonths
   );
 
-  // amount.innerHTML = initialValue;
-  amountReport.innerHTML = Number(initialValue).toLocaleString('es-co');
-  ModalAmountReport.innerHTML = Number(initialValue).toLocaleString('es-co');
+  // amount.innerHTML = rawValue;
+  amountReport.innerHTML = Number(rawValue).toLocaleString("es-co");
+  ModalAmountReport.innerHTML = Number(rawValue).toLocaleString("es-co");
   monthReport.innerHTML = initialMonths;
   ModalMonthReport.innerHTML = initialMonths;
   // monthsLabel.innerHTML = initialMonths;
   interestMVLabel.innerHTML = interestMV + "%";
-  total.innerHTML = Number(totalAmount).toLocaleString('es-co');
-  ModalTotal.innerHTML = Number(totalAmount).toLocaleString('es-co');
+  total.innerHTML = Number(totalAmount).toLocaleString("es-co");
+  ModalTotal.innerHTML = Number(totalAmount).toLocaleString("es-co");
   ModalDateFirstPayment.innerHTML = calcularFecha();
-  ModalFianza.innerHTML = Number(service).toLocaleString('es-co');
-  ModalTotalFees.innerHTML = Number((totalAmount * initialMonths) - initialValue - service).toLocaleString('es-co');
+  ModalFianza.innerHTML = Number(service).toLocaleString("es-co");
+  ModalTotalFees.innerHTML = Number(
+    totalAmount * initialMonths - rawValue - service
+  ).toLocaleString("es-co");
   // serviceReport.innerHTML = service.toLocaleString('es-co');
 }
 
@@ -87,17 +89,29 @@ function calcularFecha() {
   const año = hoy.getFullYear();
 
   let fechaResultado;
-  const opciones = { day: '2-digit', month: 'short', year: 'numeric' };
+  const opciones = { day: "2-digit", month: "short", year: "numeric" };
 
   if (dia >= 1 && dia <= 10) {
-      fechaResultado = new Date(año, mes + 1, 5);
+    fechaResultado = new Date(año, mes + 1, 5);
   } else if (dia >= 10 && dia <= 20) {
-      fechaResultado = new Date(año, mes + 1, 15);
+    fechaResultado = new Date(año, mes + 1, 15);
   } else {
-      fechaResultado = new Date(año, mes + 1, 25);
+    fechaResultado = new Date(año, mes + 1, 25);
   }
 
-  return fechaResultado.toLocaleDateString('es-ES', opciones);
+  return fechaResultado.toLocaleDateString("es-ES", opciones);
+}
+
+function formatCurrency(input) {
+  let value = input.value.replace(/[^0-9]/g, "");
+  value = parseInt(value, 10);
+  rawValue = value;
+
+  if (!isNaN(value)) {
+    input.value = "$" + value.toLocaleString("es-CO");
+  } else {
+    input.value = "";
+  }
 }
 
 // Event listener para el slider principal
@@ -116,7 +130,7 @@ sliderValues.addEventListener("input", function () {
 // Event listener para el slider de meses
 sliderMonths.addEventListener("input", function () {
   initialMonths = this.value;
-  monthsLabel.innerHTML = this.value;
+  // monthsLabel.innerHTML = this.value;
   monthReport.innerHTML = this.value;
   updateValues();
 });
@@ -125,18 +139,22 @@ sliderMonths.addEventListener("input", function () {
 updateValues();
 
 // Cuando el usuario hace clic en el botón, abre el modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // Cuando el usuario hace clic en <span> (x), cierra el modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // Cuando el usuario hace clic en cualquier lugar fuera del modal, cierra el modal
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+document.addEventListener("DOMContentLoaded", (event) => {
+  const input = document.getElementById("myRange");
+  formatCurrency(input);
+});
